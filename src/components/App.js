02324b6192131e05a7eb637d7f79008b00dca14b
billowycloud <bjkim0228@naver.com>
2021-03-09 15:58:24 +0900

    Add Common Login with firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useEffect, useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
 
 const App = () => {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 };
 export default App;
